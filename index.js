6c5cfdc31a4c62c21724c1e7961d6c6dadca2682
Ian Rivero <irivero@ngine.digital>
2019-01-18 16:54:12 -0600

    Probando comando Google

--- index.js
+++ index.js
@@ -346,7 +346,9 @@ client.on('message', async message => {
                 snekfetch = require('snekfetch'),
                 querystring = require('querystring');
 
-          if(!args) return message.reply("No ingresaste ninguna busqueda.");
+          let busqueda = args[0];
+
+          if(!busqueda) return message.reply("No ingresaste ninguna busqueda.");
 
           //Esta variable gerena una URL de nuestra busqueda/consulta (args)
           let buscarUrl = `https://www.google.com/search?q=${encodeURIComponent(args)}`;
